dc76ca58999f71dc24ade503de0d535940a25530
Javier Lago Amoedo <javierlagoamoedo.daw@gmail.com>
2022-12-04 21:30:54 +0100

    probando objeto empleado

---
+++ src/Empleado.java
@@ -0,0 +1,13 @@
+
+import java.util.Date;
+
+class Empleado {
+    final String nombre;
+    Date birthDate;
+
+    Empleado(String name, Date birthDate) {
+        this.nombre = name;
+        this.birthDate = birthDate;
+    }
+
+}
